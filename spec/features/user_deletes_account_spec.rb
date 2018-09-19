80438890ce74b8e4b6631cbce04a88165abae707
Getter508 <gettersarah@gmail.com>
2018-09-19 15:24:04 -0400

    Setup Devise for user authentication

---
+++ spec/features/user_deletes_account_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+# As an authenticated user
+# I want to delete my account
+# So that my information is no longer retained by the app
+
+# Acceptance Criteria:
+  # If I am signed in, I have an option to delete my account
---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# As a prospective user
+# I want to create an account
+# So that I can post books and review them
+
+# Acceptance Criteria:
+  # I must specify a first and last name, a valid email address, password, and
+    # password confirmation; optionally, a profile image
+  # If I don't specify the required information, I am presented with an error
+    # message
