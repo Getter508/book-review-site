8eb633fc815ed75d61fe076df2a1298e253a7d51
Getter508 <gettersarah@gmail.com>
2018-09-25 13:00:51 -0400

    Add books with validations and associations

---
+++ app/models/book.rb
@@ -0,0 +1,23 @@
+require 'date'
+
+class YearValidator < ActiveModel::Validator
+  def validate(record)
+    if record.year.to_i > Date.today.year
+      record.errors[:base] << "Publication year cannot be in the future"
+    end
+  end
+end
+
+class Book < ApplicationRecord
+  GENRES = ["Drama", "Mystery", "Romance", "Fantasy", "SciFi", "Non-Fiction"]
+
+  belongs_to :user
+
+  validates_presence_of :title, :author, :user_id
+  validates :user_id, numericality: { only_integer: true }
+  validates :genre, inclusion: { in: GENRES }
+  validates :synopsis, length: { maximum: 500 }
+  validates :year, length: { is: 4 }, numericality: { only_integer: true },
+    allow_nil: true
+  validates_with YearValidator
+end
